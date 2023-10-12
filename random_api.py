2e65f3442df0ee522d4b011c03be17214ac31749
tanner-ray-martin <tanner.ray.martin.123@gmail.com>
2023-10-12 10:57:31 -0500

    printing url

--- random_api.py
+++ random_api.py
@@ -7,5 +7,6 @@ api_url = choice(random_json_urls)
 
 api_response = helpers.hit_api(api_url)
 
+print(api_url)
 for key, value in helpers.iter_data(api_response, parent=">>> "):
     print(key.split(".")[-1]+":", value)
